12e67ffd3700fbada5fc28b9813d376f933ee735
MelikaE1380 <melikaeskandarzadeh9@gmail.com>
2025-03-21 05:19:35 -0600

    develop branch created

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <p>ملیکا اسکندرزاده</p>
+        <p>develop branch</p>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
